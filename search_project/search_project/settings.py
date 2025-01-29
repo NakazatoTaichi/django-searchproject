b8c01268f53f76a1b4f79644185cbde940bab62e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-01-30 02:54:20 +0900

    一旦コミット

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -1,18 +1,18 @@
-import os
 from pathlib import Path
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&ox*4*o5-8x)^re$cnv9!q58!eu#pu3caf3!#i_km05_fqp4m0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'search_app.apps.SearchAppConfig',
+    'accounts.apps.AccountsConfig',
     'search_app',
     'accounts'
 ]
@@ -61,19 +63,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'search_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'search_project_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': env.db(),
 }
 
 
