d99e1b2b4dd4c1c75311ca75b54c96e2fb10d180
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-01-31 15:41:48 +0900

    サーバー立ち上げた後に表示される画面を指定

--- search_project/search_project/urls.py
+++ search_project/search_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_runserver(request):
+    return redirect('accounts:login')
 
 urlpatterns = [
+    path('', redirect_to_runserver),
     path('admin/', admin.site.urls),
     path('search/', include('search_app.urls')),
     path('', include('accounts.urls')),
