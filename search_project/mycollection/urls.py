2ad666a295e3c2d30f0ecddb0fa2e083e5855a1e
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-02-01 23:15:08 +0900

    コレクション登録画面の表示

--- search_project/mycollection/urls.py
+++ search_project/mycollection/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'mycollection'
 
 urlpatterns = [
     path('home', views.collection_home, name='home'),
-]
+    path('register', views.collection_register, name='collection_register'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
