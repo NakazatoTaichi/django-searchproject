bc02ff95b28b057a7987f9bfaf1ea57328c4accb
NakazatoTaichi <taichi.nakazato0521@gmail.com>
2025-02-04 05:50:28 +0900

    絞り込み検索と並び替え順で指定できるようにした

--- search_project/mycollection/views.py
+++ search_project/mycollection/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render , redirect
-from .forms import MyCollectionForm, CollectionCategoryForm
+from .forms import CollectionSearchForm, MyCollectionForm, CollectionCategoryForm
 from django.contrib import messages
 from .models import MyCollection
 from django.core.paginator import Paginator
@@ -10,16 +10,46 @@ from django.contrib.auth.decorators import login_required
 def collection_home(request):
     mycollections = MyCollection.objects.filter(user=request.user)
 
+    # コレクション名検索
     query = request.GET.get('query')
     if query:
         mycollections = mycollections.filter(name__icontains=query)
+
+    # 絞り込み検索
+    form = CollectionSearchForm(user=request.user, data=request.GET)
+    if form.is_valid():
+        category = form.cleaned_data.get('collection_category')
+        if category:
+            mycollections = mycollections.filter(collection_category=category)
+
+    get_date = request.GET.get('get_date')
+    if get_date:
+        mycollections = mycollections.filter(get_date__exact=get_date)
+
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    if price_min and price_max:
+        mycollections = mycollections.filter(price__gte=price_min, price__lte=price_max)
+    elif price_min:
+        mycollections = mycollections.filter(price__gte=price_min)
+    elif price_max:
+        mycollections = mycollections.filter(price__lte=price_max)
+
+    # 並び替え順
+    order = request.GET.get('order')
+    if order == 'name_order':
+        mycollections = mycollections.order_by('name')
+    elif order == 'register_order_des':
+        mycollections = mycollections.order_by('-created_at')
+    elif order == 'register_order_asc':
+        mycollections = mycollections.order_by('created_at')
 
     # ページネーション
     paginator = Paginator(mycollections, 8)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'home.html', {'page_obj': page_obj})
+    return render(request, 'home.html', {'form': form, 'page_obj': page_obj})
 
 @login_required
 def collection_register(request):
